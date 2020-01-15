583415aff36efa848d7d2d7d0af4583e5fa9141e
Marc Laroussini <marclaroussini@alum.calarts.edu>
2020-01-15 15:01:22 -0800

    added !addplayer

---
+++ commands/addplayer.js
@@ -0,0 +1,49 @@
+module.exports = {
+	name: 'addplayer',
+	description: 'Adds a player to the game',
+    format: "!addplayer <username> <Character>",
+    guildonly: true,
+    gmonly: true,
+	execute(client, message, args) {
+
+        if (args.length == 0) {
+            return message.channel.send("You need to enter a user.");
+        }
+
+        const inputusername = args.shift().toLowerCase();
+        const characterName = args.join(" ");
+
+        if (characterName.length == 0) {
+            return message.channel.send("You need to enter a character name.");
+        }
+
+        //find player based on input
+        var player;
+        message.guild.members.forEach(function(m) {
+            //console.log(m.user.username.toLowerCase() , inputusername, m.user.username.toLowerCase().includes(inputusername));
+            if (m.user.username.toLowerCase().includes(inputusername)) {
+                player = m.user;
+            }
+        })
+            
+        //Notify if invalid input for user
+        if (player == undefined) {
+            return message.channel.send("Invalid username: " + inputusername);
+        }
+
+
+        var players = client.data.get("PLAYER_DATA");
+
+        if (players == undefined) {
+            players = [];
+        }
+
+        players.push({player: player, name: player.username, character: characterName, area: undefined, move: undefined});         
+        
+        client.data.set("PLAYER_DATA", players);
+
+        message.channel.send("Player added! " + players.length + " players so far.\n__**PLAYER LIST:**__\n"
+             + players.map(p => "**" + p.name + "**, " + p.character).join('\n'));
+        
+	}
+};
