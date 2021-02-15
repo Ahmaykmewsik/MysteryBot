23cb4f2ff2993f61120a244a14a3cb1c080103a6
Ahmaykmewsik <marclaroussini@alum.calarts.edu>
2021-02-14 19:41:48 -0500

    disable movement cause its still buggy

--- utilities/sendPassMessages.js
+++ utilities/sendPassMessages.js
@@ -4,8 +4,10 @@ module.exports = {
             //Don't do anything if we're in or going to multiarea mode
             if ((Array.isArray(player.area) && player.area.length > 1) || player.area == undefined) {return;}
             if ((Array.isArray(player.move) && player.move.length > 1) || player.area == undefined) {return;}
+            if (player.move == undefined) {return;}
+
             //find players where they're passing each other and send them a notification DM that it happened
-            swappers = players.filter(p => (player.area.includes(p.move[0])) && (player.move.includes(p.area[0])));
+            swappers = players.filter(p => (player.area.includes(p.move)) && (player.move.includes(p.area)));
             if (swappers.length > 0) {
                 const swappersString = swappers.map(s => s.character).join(', ');
                 playerobject = members.find(m => m.user.username == player.name)
